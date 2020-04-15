a673b08e4a95c0f848ceeb7d7e1894b0a18b94e5
Matthew <mattwalter35@gmail.com>
2020-04-15 12:23:17 -0400

    Initial Commit

---
+++ Shout/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Shout
+//
+//  Created by Matthew Walter on 4/15/20.
+//  Copyright © 2020 Matthew Walter. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
